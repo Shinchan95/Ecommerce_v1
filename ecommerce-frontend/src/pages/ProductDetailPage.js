93f5cb86546bd0a8932e3dc94028a13bf6c3f5e4
Shinchan95 <shriganesh.bhandari09@gmail.com>
2023-11-21 11:42:08 +0530

    Final Commit

---
+++ ecommerce-frontend/src/pages/ProductDetailPage.js
@@ -0,0 +1,16 @@
+import NavBar from "../features/navbar/Navbar";
+import ProductDetails from "../features/product/components/ProductDetails";
+import Footer from "../features/common/Footer";
+
+function ProductDetailPage() {
+    return ( 
+        <div>
+            <NavBar>
+                <ProductDetails></ProductDetails>
+            </NavBar>
+            <Footer></Footer>
+        </div>
+     );
+}
+
+export default ProductDetailPage;
